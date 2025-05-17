8d637ff3b9cb81f7290b86bcba32fdfec8b4b4c8
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-05-17 19:45:11 +0530

    added local buttton and title animation components

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = ({title,id,leftIcon,rightIcon,containerClass}) => {
+    
+  return (
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer rounded-full px-7 py-3 ${containerClass}`}>
+        {leftIcon}
+        {console.log("hi"+title)}
+        <span className='mx-1 relative inline-flex overflow-hidden font-general text-xs uppercase text-gray-800 font-semibold tracking-wider'>
+                {title}
+        </span>
+        {rightIcon}
+    </button>
+  )
+}
+
+export default Button;

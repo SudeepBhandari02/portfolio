20d1a86213485e7d1466f64da9f8a1165905d909
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-05-17 19:47:16 +0530

    Updated App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import About from './components/About';
 import Hero from './components/Hero';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      
+      <NavBar/>
       <Hero/>
+      <About/>
+
+      <section className='z-10 min-h-screen bg-blue-500'></section>
     </main>
   )
 }
-
 export default App;

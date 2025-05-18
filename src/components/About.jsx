8dac17a7adca3ea2da70662109c751ef663f382d
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-05-18 12:22:45 +0530

    updated about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
+import devWorldImage from "../assets/images/devWorldImage.png"
 
 import AnimatedTitle from "./AnimatedTitle";
 
@@ -19,12 +20,63 @@ const About = () => {
       },
     });
 
+    clipAnimation.from(".mask-clip-path", {
+        clipPath:"polygon(29% 10%, 76% 2%, 76% 91%, 24% 79%)",
+    });
+
     clipAnimation.to(".mask-clip-path", {
       width: "100vw",
       height: "100vh",
       borderRadius: 0,
     });
   });
+
+//     useGSAP(() => {
+//     gsap.set(".mask-clip-path", {
+//       clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", // Start: full box
+//     });
+
+//     gsap.to(".mask-clip-path", {
+//       clipPath: "polygon(14% 14%, 72% 8%, 90% 87%, 7% 90%)", // End shape
+//       scrollTrigger: {
+//         trigger: "#clip",
+//         start: "top center", // animation starts when box hits center of viewport
+//         end: "bottom top",   // ends when box leaves top
+//         scrub: true,
+//       },
+//       ease: "power1.inOut",
+//     });
+//   }, []);
+
+  // useGSAP(() => {
+  //   gsap.to(".mask-clip-path", {
+  //     scale: 1.2,
+  //     scrollTrigger: {
+  //       trigger: "#clip",
+  //       start: 'top bottom', // image enters viewport
+  //       end: 'top top', // image reaches top
+  //       scrub: true,
+  //     },
+  //   });
+  // }, []);
+
+  // Mouse-based movement
+  const handleMouseMove = (e) => {
+    const { innerWidth, innerHeight } = window;
+    const rotateX = (e.clientY / innerHeight - 0.5) * 20;
+    const rotateY = (e.clientX / innerWidth - 0.5) * 20;
+    const translateX = (e.clientX / innerWidth - 0.5) * 20;
+    const translateY = (e.clientY / innerHeight - 0.5) * 20;
+
+    gsap.to(".mask-clip-path", {
+      rotateX: -rotateX,
+      rotateY: rotateY,
+      translateX,
+      translateY,
+      duration: 0.4,
+      ease: 'power2.out',
+    });
+  };
 
   return (
     <div id="about" className="min-h-screen w-screen">
@@ -50,10 +102,10 @@ const About = () => {
         </div>
       </div>
 
-      <div className="h-dvh w-screen" id="clip">
+      <div className="relative h-dvh w-screen" id="clip" onMouseMove={handleMouseMove}>
         <div className="mask-clip-path about-image">
           <img
-            src="img/about.webp"
+            src={devWorldImage}
             alt="Background"
             className="absolute left-0 top-0 size-full object-cover"
           />

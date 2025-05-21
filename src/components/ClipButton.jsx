f5b82c269f78fc43a64f198ccc38297eda931f7b
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-05-21 23:54:26 +0530

    updated ClipButton to have rounded corners

--- src/components/ClipButton.jsx
+++ src/components/ClipButton.jsx
@@ -5,10 +5,10 @@ import ButtonSound from "/audio/buttonSound.mp3";
 
 gsap.registerPlugin(MorphSVGPlugin);
 
-const ClipButton = ({title,leftIcon,rightIcon}) => {
+const ClipButton = ({ title, leftIcon, rightIcon }) => {
   const pathRef = useRef(null);
   const audioRef = useRef(null);
-  const clipPathId = useId(); 
+  const clipPathId = useId();
 
   const originalPath = `
     M0.9625,0.05 V0.95 H0.0375 V0.05 H0.9625 Z
@@ -34,8 +34,7 @@ const ClipButton = ({title,leftIcon,rightIcon}) => {
     return () => gsap.killTweensOf(pathRef.current);
   }, [originalPath]);
 
-  const handleEnter = () =>{
-
+  const handleEnter = () => {
     gsap.to(pathRef.current, {
       duration: 0.5,
       morphSVG: clippedPath,
@@ -45,7 +44,7 @@ const ClipButton = ({title,leftIcon,rightIcon}) => {
     const audio = audioRef.current;
     audio.currentTime = 0;
     audio.play();
-  }
+  };
 
   const handleLeave = () =>
     gsap.to(pathRef.current, {
@@ -73,14 +72,32 @@ const ClipButton = ({title,leftIcon,rightIcon}) => {
         }}
         className="text-gray-800 px-6 py-3 text-lg font-robert-regular tracking-wider font-semibold group rounded-lg transition-all duration-300 cursor-pointer hover:bg-black hover:text-[#5542ff]"
       >
-        <button className="px-2 py-1 flex items-center justify-center gap-2 rounded-xl border-1 border-gray-950 group-hover:border-none whitespace-nowrap">
+        {/* <button className="px-2 py-1 flex items-center justify-center gap-2 rounded-xl border-1 border-gray-950 group-hover:border-none whitespace-nowrap">
           {leftIcon?leftIcon:""}
-          {title}
+          <p className="group-hover:-translate-y-7 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]">{title}</p>
           {rightIcon?rightIcon:""}
+        </button> */}
+        <button class="cursor-pointer  px-2 py-1 rounded-xl border-[1px] border-slate-800 text-gray-900 group-hover:text-[#5542ff] font-semibold group-hover:border-none whitespace-nowrap flex items-center justify-center shadow-lg">
+          <div class="relative overflow-hidden">
+            <p class="group-hover:-translate-y-7 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)] flex items-center justify-center gap-2">
+              {leftIcon?leftIcon:""}
+              {title}
+              {rightIcon?rightIcon:""}
+            </p>
+            <p class="absolute top-7 left-0 group-hover:top-0 duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)]">
+              {leftIcon?leftIcon:""}
+              {title}
+              {rightIcon?rightIcon:""}
+            </p>
+          </div>
         </button>
-        
       </div>
-      <audio ref={audioRef} src={ButtonSound} preload="auto" className="hidden"/>
+      <audio
+        ref={audioRef}
+        src={ButtonSound}
+        preload="auto"
+        className="hidden"
+      />
     </div>
   );
 };

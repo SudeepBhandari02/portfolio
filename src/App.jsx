7098a66b27d3fbd57fe1caf9931d6a62e2442bf4
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-05-22 23:15:39 +0530

    updated App.jsx with all the necessary components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+import { useEffect } from 'react';
+
 import About from './components/About';
+import Contact from './components/Contact';
+import Features from './components/Features';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import NavBar from './components/Navbar';
+import Skills from './components/Skills';
 
 function App() {
+
+  useEffect(() => {
+  const handleLoad = () => {
+    const pre = document.getElementById('preloader');
+    if (pre) pre.style.display = 'none';
+  };
+
+  if (document.readyState === 'complete') {
+    handleLoad();
+  } else {
+    window.addEventListener('load', handleLoad);
+  }
+
+  return () => {
+    window.removeEventListener('load', handleLoad);
+  };
+}, []);
+
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className='relative min-h-screen w-full overflow-x-hidden'>
       
       <NavBar/>
       <Hero/>
       <About/>
-
+      <Features/>
+      <Skills/>
+      <Contact/>
+      <Footer/>
       <section className='z-10 min-h-screen bg-blue-500'></section>
     </main>
   )
